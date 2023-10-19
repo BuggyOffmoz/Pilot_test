8c306f3e72317ed23d1856d75b0a9f75924b04db
BuggyOffmoz <143240799+BuggyOffmoz@users.noreply.github.com>
2023-10-19 14:10:09 -0300

    Update Commit

--- Scenes/Components/SheepDetector/SheepDetectorComponentSCRIPT.gd
+++ Scenes/Components/SheepDetector/SheepDetectorComponentSCRIPT.gd
@@ -5,9 +5,14 @@ extends Area2D
 
 
 
-
 func _on_body_entered(body):
-	parent_reference.sheep_detected(body)
+	if parent_reference.has_method("sheep_detected"):
+		parent_reference.sheep_detected(body)
+	else:
+		pass
 
 func _on_body_exited(body):
-	parent_reference.sheep_scape(body)
+	if parent_reference.has_method("sheep_detected"):
+		parent_reference.sheep_scape(body)
+	else:
+		pass
--- Scenes/Components/SheepDetector/WorldBaseObject.gd
+++ Scenes/Components/SheepDetector/WorldBaseObject.gd
@@ -5,16 +5,15 @@ class_name WorldBaseObject
 
 var array_sheeps_detected: Array[CharacterBody2D]
 
+
 func sheep_detected(body):
 	if array_sheeps_detected.has(body):
 		pass
 	else:
 		array_sheeps_detected.insert(array_sheeps_detected.size(),body)
-	print(array_sheeps_detected)
 
 func sheep_scape(body):
 	if array_sheeps_detected.has(body):
 		array_sheeps_detected.remove_at(array_sheeps_detected.find(body))
 	else:
 		pass
-	print(array_sheeps_detected)
